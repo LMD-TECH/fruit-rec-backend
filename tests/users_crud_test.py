dc86ffc4b241e83e5ea816c73430227773c02604
Alpha Oumar Diallo <aljibree.diallo@gmail.com>
2025-03-18 23:43:49 +0000

    mise à jour jenkinsfile

--- tests/users_crud_test.py
+++ tests/users_crud_test.py
@@ -8,7 +8,7 @@ import logging
 user_mock = {
     "nom_famille": "Dupont",
     "prenom": "Jean",
-    "email": "lumeida.tech@gmail.com",
+    "email": "lumeida.tech@gmail",
     "numero_telephone": "22177777777",
     "mot_de_passe": "test",
 }
