f0bfe57094dd15043c0ab7679ae455f7bd8a61bd
CodeANGEL <codeangel223@gmail.com>
2025-03-29 00:01:13 +0000

    Update constantes.py

--- tests/constantes.py
+++ tests/constantes.py
@@ -3,7 +3,7 @@
 user_mock = {
     "nom_famille": "Dupont",
     "prenom": "Jean",
-    "email": "lumeida.tech@gmail.com",
+    "email": "lumeida.tech@gmail",
     "numero_telephone": "22177777777",
     "mot_de_passe": "test",
 }
